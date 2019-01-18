b247a7657b871757b77d1f146bd99354a8b56bbe
Freddie Ridell <contact@freddieridell.com>
2019-01-18 16:19:52 +0000

    actually got a lot of feature parity already

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
-fn get_host_name(): str {
- "%{%F{red}%}%m";
+fn get_host_name() -> String {
+ String::from("%{%F{red}%}%n@%m")
+}
+
+fn get_current_dir() -> String {
+    String::from("%{%F{green}%}%~")
+}
+
+fn get_time() -> String {
+    String::from("%{%F{white}%}%D{%a %Y-%m-%d %T}")
+}
+
+fn get_repo() -> String {
+    String::from("%{%F{white}%}(%{%F{magenta}%}repo%{%f%})")
+}
+
+fn get_newline() -> String {
+    String::from("\n%f$ ")
 }
 
 fn main() {
-    println!("{}", get_host_name());
+    println!("{} {} {} {}{}", get_host_name(), get_time(), get_repo(), get_current_dir(), get_newline());
 }
